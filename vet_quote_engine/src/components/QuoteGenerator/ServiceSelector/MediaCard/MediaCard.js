de4d1c70cc7e17422c20e0c9dc256169d8ea7e7b
Devin <devster99@gmail.com>
2023-12-04 23:23:36 -0700

    Finished migrating quote_generator to MUI. Added logic for formatting based on new Cards and selected state. Updated database to include image in services. Service Image is now rendered with each componenent. Will update Sizes later!

--- vet_quote_engine/src/components/QuoteGenerator/ServiceSelector/MediaCard/MediaCard.js
+++ vet_quote_engine/src/components/QuoteGenerator/ServiceSelector/MediaCard/MediaCard.js
@@ -8,13 +8,28 @@ import Typography from "@mui/material/Typography";
 import { Box } from "@mui/material";
 import AppContext from "../../../../context/AppContext";
 import { useContext } from "react";
+import "./MediaCard.css";
 
 export default function MediaCard() {
-const { services, selectedServices, setSelectedServices } = useContext(AppContext);
+  const { services, selectedServices, setSelectedServices } =
+    useContext(AppContext);
+  const selectedClass = "selected_service";
+  const handleSelect = (service) => {
+    setSelectedServices([...selectedServices, service.id]);
+  };
+  const handleDeselect = (id) => {
+    const indexToRemove = selectedServices.findIndex(
+      (element) => element === id
+    );
+    selectedServices.splice(indexToRemove, 1);
+    setSelectedServices([...selectedServices]);
+  };
+
   return (
-    <Box>
+    <Box className="service-container">
       {/* Finds out if service is in selected services */}
       {services.map((service) => {
+        console.log(service.image);
         let isSelected = false;
         if (
           selectedServices.findIndex((element) => element === service.id) >= 0
@@ -25,53 +40,76 @@ const { services, selectedServices, setSelectedServices } = useContext(AppContex
         //      Conditional rendering if a service is selected vs not selected.
         if (isSelected) {
           return (
-            <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image={service.image}
-              title="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-      
+            <Card sx={{ maxWidth: 345 }} className={selectedClass}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image={require(`./images/${service.image}`)}
+                title={`${service.name}`}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {service.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {service.description}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="text.tertiary"
+                  fontWeight={"bold"}
+                >
+                  {`$${service.price}`}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  size="small"
+                  color="error"
+                  variant="outlined"
+                  onClick={() => handleDeselect(service.id)}
+                >
+                  Deselect
+                </Button>
+              </CardActions>
+            </Card>
           );
         } else {
           return (
-            <div>
-              <Card sx={{ maxWidth: 345 }}>
+            <Box>
+              <Card sx={{ maxWidth: 345 }} className="service">
                 <CardMedia
                   sx={{ height: 140 }}
-                  image={service.image}
-                  title="green iguana"
+                  image={require(`./images/${service.image}`)}
+                  title={service.name}
+                  component="img"
                 />
                 <CardContent>
                   <Typography gutterBottom variant="h5" component="div">
-                    Lizard
+                    {service.name}
                   </Typography>
                   <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with
-                    over 6,000 species, ranging across all continents except
-                    Antarctica
+                    {service.description}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="text.tertiary"
+                    fontWeight={"bold"}
+                  >
+                    {`$${service.price}`}
                   </Typography>
                 </CardContent>
                 <CardActions>
-                  <Button size="small">Share</Button>
-                  <Button size="small">Learn More</Button>
+                  <Button
+                    size="small"
+                    color="success"
+                    variant="outlined"
+                    onClick={() => handleSelect(service)}
+                  >
+                    Select
+                  </Button>
                 </CardActions>
               </Card>
-            </div>
+            </Box>
           );
         }
       })}
--- vet_quote_engine/src/components/QuoteGenerator/ServiceSelector/ServiceSelector.js
+++ vet_quote_engine/src/components/QuoteGenerator/ServiceSelector/ServiceSelector.js
@@ -18,13 +18,6 @@ const ServiceSelector = () => {
   // Adding to selectedServices State
   const handleSelect = (service) => {
     setSelectedServices([...selectedServices, service.id]);
-    // setRunningTotal([...runningTotal, service.price]);
-    // old and unneeded code? \/
-    // if (selectedServices.findIndex((element) => element === id) === -1) {
-    //   setSelectedServices([...selectedServices, id]);
-    // } else {
-    //   console.log("This service has already been added");
-    // }
   };
 
   // Removing from selectedServices State
@@ -38,54 +31,7 @@ const ServiceSelector = () => {
 
   return (
     <Paper>
-      {/* <h3>Total: {`$${runningTotal}`}</h3> */}
-      {/* <button onClick={() => showSelected()}>Show Selected</button> */}
-      <Box className="service-container">
-        {/* Finds out if service is in selected services */}
-        {services.map((service) => {
-          let isSelected = false;
-          if (
-            selectedServices.findIndex((element) => element === service.id) >= 0
-          ) {
-            isSelected = true;
-          }
-
-          //      Conditional rendering if a service is selected vs not selected.
-          if (isSelected) {
-            return (
-              <Card className={selectedClass}>
-                <h3>{service.name}</h3>
-                <p>{service.description}</p>
-                <p>{service.price}</p>
-                <button
-                  className="deselect-button"
-                  onClick={() => handleDeselect(service.id)}
-                >
-                  REMOVE
-                </button>
-              </Card>
-            );
-          } else {
-            return (
-              <div>
-                <MediaCard />
-
-                <Card className="service">
-                  <h3>{service.name}</h3>
-                  <p>{service.description}</p>
-                  <p>{service.price}</p>
-                  <button
-                    className="select-button"
-                    onClick={() => handleSelect(service)}
-                  >
-                    SELECT
-                  </button>
-                </Card>
-              </div>
-            );
-          }
-        })}
-      </Box>
+      <MediaCard />
     </Paper>
   );
 };
