46ebec1a70f4a3bc6a7e1a85dd239e901d7b192c
Devin <devster99@gmail.com>
2023-12-06 08:49:47 -0700

    Fixed loop with jacob

--- vet_quote_engine/src/components/QuoteDisplay/QuoteDisplay.js
+++ vet_quote_engine/src/components/QuoteDisplay/QuoteDisplay.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import "./QuoteDisplay.css";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import AppContext from "../../context/AppContext";
 import { Box, Paper, Button, Typography } from "@mui/material";
 import AdminRender from "./AdminRender";
@@ -12,7 +12,6 @@ const QuoteDisplay = () => {
   const { id } = useParams();
   const { user, setUser } = useContext(AppContext);
   const [services, setServices] = useState();
-  let i = 0;
 
   const toggleAdmin = () => {
     if (user.tier === "admin") {
@@ -22,24 +21,17 @@ const QuoteDisplay = () => {
     }
   };
 
-//   const getQuote = async (id) => {
-//     if (i >= 10) {
-//       return;
-//     } else {
-//       const response = await axios.get(`http://localhost:4001/v1/quotes/${id}`);
-//       const quoteServices = response.data.quoteServices;
-//       const updateServiceState = (services) => {
-//         setServices(services);
-//         console.log('inside services')
-//       };
-//       updateServiceState(quoteServices);
-//       console.log("Services:", services);
-//       console.log("i",i);
-//       i++;
-//     }
-//   };
-//   getQuote(id);
+  useEffect(() => {
+    const getQuote = async (id) => {
+        const response = await axios.get(`http://localhost:4001/v1/quotes/${id}`);
+        const quoteServices = response.data.quoteServices;
+        setServices(quoteServices);
+      };
+      getQuote(id);
+  }, [id])
 
+
+  console.log(services);
   if (user.tier === "admin") {
     return <AdminRender toggleAdmin={toggleAdmin} />;
   } else {
